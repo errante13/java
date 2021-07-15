74527afaf6fffa35479d9258d03081ec5ab9a1c7
errante13 <luislagos62@gmail.com>
2021-07-15 13:40:49 -0400

    holaMundo
    primera tarea java fundamentos

---
+++ prueba.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author luisr
+ */
+public class prueba {
+    
+    public static void main (String[]args){
+        
+      int edad = 29; 
+      String nombre = "luis"; 
+      String ciudad = "santiago"; 
+      
+      
+        System.out.println("hola, mi nombre es: "+nombre);
+        System.out.println("soy de "+ ciudad);
+         System.out.println("y tengo: "+ edad+ " años");
+        
+    }
+}
